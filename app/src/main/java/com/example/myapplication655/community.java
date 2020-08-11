5482336f2fed8ed13535814e2e10297bfd37d594
yujin5959 <jinjin17104@naver.com>
2020-08-11 11:20:30 +0900

    이게 로그인 화면이다 지수야

--- app/src/main/java/com/example/myapplication655/community.java
+++ app/src/main/java/com/example/myapplication655/community.java
@@ -3,6 +3,7 @@ package com.example.myapplication655;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class community extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class community extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_community);
+
+        Button login_success = findViewById(R.id.login_success);
     }
 }
